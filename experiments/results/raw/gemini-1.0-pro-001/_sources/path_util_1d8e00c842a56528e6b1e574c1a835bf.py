790fe321907fa772f90adaa952775ce9035f7e7c
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2024-03-16 21:33:53 +0100

    add raw and cleansed gemini-pro results

---
+++ experiments/results/raw/gemini-1.0-pro-001/_sources/path_util_1d8e00c842a56528e6b1e574c1a835bf.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from typing import Final
+
+project_root_dir: Final[Path] = Path(__file__).parent.parent
+data_dir: Final[Path] = project_root_dir / "data"
+results_dir: Final[Path] = project_root_dir / "results"
+experiment_results_dir: Final[Path] = project_root_dir / "experiments" / "results"
+cleansed_experiment_results_dir: Final[Path] = experiment_results_dir / "cleansed"
+
+
+def get_data_path(language: str) -> Path:
+    path = data_dir / "preprocessed" / f"dataset_{language}.csv"
+    assert path.exists(), f"dataset for language '{language}' not found"
+    return path
--- scripts/results/validate.py
+++ scripts/results/validate.py
@@ -59,9 +59,8 @@ def _validate_result(model: str, language: str) -> Optional[list[int]]:
         return None
     erroneous_games = []
     for game_idx, answers in enumerate(games):
-        if answers == "PROMPT BLOCKED":
+        if type(answers) is str and answers.startswith("PROMPT BLOCKED"):
             _log_error(model, language, game_idx, error="prompt blocked")
-            erroneous_games.append(game_idx)
             continue
         if type(answers) is not list:
             _log_error(model, language, game_idx, error=f"unexpected type of answer list (is {type(answers)}, should be list)")
