c4bee2062de8eb834d9bbea8e3594e0df3e57b95
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2024-04-28 18:46:38 +0200

    remove unused reinforcement prompts

--- src/model/model.py
+++ src/model/model.py
@@ -16,19 +16,16 @@ class Model(ABC):
     def __init__(self, language: str, _log: logging.Logger) -> None:
         self._dry_run = not bool(os.getenv("NO_DRY_RUN", False))
         if not self.dry_run:
-            warnings.warn(f"Running model NOT in dry mode! This will consume API tokens and may cost money. MAKE SURE TO RUN IN DRY MODE FIRST TO GET AN ESTIMATE!")
+            warnings.warn(
+                f"Running model NOT in dry mode! This will consume API tokens and may cost money. MAKE SURE TO RUN IN DRY MODE "
+                f"FIRST TO GET AN ESTIMATE!"
+            )
 
         with open(path_util.data_dir / "system_prompts.json") as f:
             system_prompts = json.load(f)
         system_prompt = system_prompts.get(language)
         assert system_prompt, f"no system prompt for language '{language}'"
         self._system_prompt = system_prompt
-
-        with open(path_util.data_dir / "reinforcement_prompts.json") as f:
-            reinforcement_prompts = json.load(f)
-        reinforcement_prompt = reinforcement_prompts.get(language)
-        assert reinforcement_prompt, f"no reinforcement prompt for language '{language}'"
-        self._reinforcement_prompt = reinforcement_prompt
 
         self._num_input_tokens = 0
         self._num_output_tokens = 0
@@ -53,7 +50,3 @@ class Model(ABC):
     @property
     def system_prompt(self) -> str:
         return self._system_prompt
-
-    @property
-    def reinforcement_prompt(self) -> str:
-        return self._reinforcement_prompt
