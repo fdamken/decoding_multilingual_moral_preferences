b3b2454419a1fc6f09664af1cabe4544bdc2fd31
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2024-05-04 14:18:08 +0200

    lots of fixes and changed to fig3

--- scripts/moral_machine/Fig3/figure3b.py
+++ scripts/moral_machine/Fig3/figure3b.py
@@ -1,8 +1,9 @@
-import matplotlib.font_manager as fm
-import matplotlib.patches as mpatches
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import tikzplotlib
 from scipy.cluster import hierarchy as hch
 
 subtext = ["Preference for Inaction", "Sparing Pedestrians", "Sparing Females", "Sparing the Fit",
@@ -30,7 +31,7 @@ def vectorize(df):
         Output: Numpy Array
     """
 
-    X = df.values[:, 4:].astype(float)
+    X = df.values[:, 1:].astype(float)
 
     # Normalize Values
     X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
@@ -92,10 +93,46 @@ def create_d3_dendogram(df, leaf_color=None, save_csv=True):
     return d3_dendo_tree_df
 
 
-def radar_plot(df, savefig=True):
-    prefs = df.columns[4:]
+def radar_plot(title, df, savefig=True):
     X = vectorize(df)
     Z = hch.linkage(X, method='ward')
+
+    language_map = {
+        "ar": "Arabic",
+        "de": "German",
+        "en": "English",
+        "es": "Spanish",
+        "fr": "French",
+        "ja": "Japanese",
+        "kr": "Korean",
+        "pt": "Portuguese",
+        "ru": "Russian",
+        "zh": "Chinese",
+    }
+    WESTERN = 0
+    EASTERN = 1
+    _language_clusters = {
+        "ar": EASTERN,
+        "de": WESTERN,
+        "en": WESTERN,
+        "es": WESTERN,
+        "fr": WESTERN,
+        "ja": EASTERN,
+        "kr": EASTERN,
+        "pt": WESTERN,
+        "ru": EASTERN,
+        "zh": EASTERN,
+    }
+
+    # noinspection PyTypeChecker
+    hch.dendrogram(Z, labels=[language_map[l] for l in df["Country"].values])
+    plt.xticks(rotation=90)
+    plt.yticks([])
+    plt.title(title)
+    plt.tight_layout()
+    tikzplotlib.clean_figure()
+    tikzplotlib.save("dendrogram.tikz")
+    plt.show()
 
     rootnode, node_list = hch.to_tree(Z, rd=True)
     N = len(node_list) - 1
@@ -122,62 +159,48 @@ def radar_plot(df, savefig=True):
 
         hierarchy = geneology[id_]
         if node.dist == 0:
-            val = float(hierarchy[2:5])
-            if val == 1.3:
-                grouping.append(0)
-            elif val == 1.5:
-                grouping.append(1)
-            elif val > 2.0:
-                grouping.append(2)
+            for language, cluster in _language_clusters.items():
+                if hierarchy.endswith(language):
+                    grouping.append(cluster)
+                    break
             else:
-                print(val)
-
-    colors = ["#CC6677", "#4477AA", "#DDCC77"]
-
-    patch1 = mpatches.Patch(color="#CC6677", alpha=0.8)
-    patch2 = mpatches.Patch(color="#4477AA", alpha=0.8)
-    patch3 = mpatches.Patch(color="#DDCC77", alpha=0.8)
-    font_prop = fm.FontProperties(fname='fonts/RobotoCondensed-Regular.ttf', size=20)
+                assert False, f"no language found for {hierarchy=}"
 
     labels = ["Western",
-              "Eastern",
-              "Southern"]
+              "Eastern"]
 
     grouping = np.array(grouping)
     grouped_X = list()
-    for g in range(3):
+    for g in range(2):
         grouped_X.append(X[::-1][grouping == g])
 
-    cluster1_means = grouped_X[0].mean(axis=0)  # Cluster 1
-    cluster2_means = grouped_X[1].mean(axis=0)  # Cluster 2
-    cluster3_means = grouped_X[2].mean(axis=0)  # Cluster 3
+    cluster1_means = grouped_X[0].mean(axis=0)
+    cluster2_means = grouped_X[1].mean(axis=0)
 
     N = len(cluster1_means)
     x_as = [n / float(N) * 2 * np.pi for n in range(N)]
 
     cluster1_means = np.append(cluster1_means, cluster1_means[:1])
     cluster2_means = np.append(cluster2_means, cluster2_means[:1])
-    cluster3_means = np.append(cluster3_means, cluster3_means[:1])
 
     x_as += x_as[:1]
 
-    clusters = np.array([cluster1_means, cluster2_means, cluster3_means])
+    clusters = np.array([cluster1_means, cluster2_means])
     min_val = clusters.min()
     clusters_adj = np.sqrt(np.square(clusters - min_val))
-    ytick_labels = [np.sqrt(np.square(i - 1 - min_val)) - .3 for i in range(4)]
 
     colors = ["#CC6677", "#4477AA", "#DDCC77"]
 
-    subtext = ["Preference \nfor Inaction", "Sparing \nPedestrians", "Sparing \nFemales", "Sparing \nthe Fit",
-               "Sparing \nthe Lawful", "Sparing \nHigher Status", "Sparing \nthe Younger", "Sparing \nMore",
-               "Sparing \nHumans"]
+    subtext = ["preference \nfor inaction", "sparing \npedestrians", "sparing \nfemales", "sparing \nthe fit",
+               "sparing \nthe lawful", "sparing \nhigher status", "sparing \nthe young", "sparing \nmore",
+               "sparing \nhumans"]
 
-    font_prop = fm.FontProperties(fname='fonts/RobotoCondensed-Regular.ttf', size=20)
-    font_axis_prop = fm.FontProperties(fname='fonts/RobotoCondensed-Regular.ttf', size=22)
-    font_title_prop = fm.FontProperties(fname='fonts/RobotoCondensed-Regular.ttf', size=36)
-
-    fig, axes = plt.subplots(1, 3, figsize=(22, 8), subplot_kw={'projection': 'polar'})
-    fig.subplots_adjust(wspace=.3)
+    fig, axes = plt.subplots(
+        1,
+        2,
+        figsize=(2 * 7.00137, 2 * 7.00137 / 2),
+        subplot_kw={'projection': 'polar'}
+    )
 
     for i, ax in enumerate(axes.ravel()):
         ax.set_theta_offset(np.pi / 2)
@@ -186,30 +209,51 @@ def radar_plot(df, savefig=True):
         ax.fill(x_as, clusters_adj[i], color=colors[i], alpha=0.4)
         ax.plot(np.linspace(0, 2 * np.pi, 1000), np.ones(1000) * -1 * min_val, color='black', linewidth=0.7)
         ax.set_xticks(x_as[:-1])
-        ax.set_xticklabels([])
-        ax.set_ylim(0, 3)
+        ax.set_xticklabels(subtext)
         ax.tick_params(axis='y', width=10, which='major')
-        y_ticks = np.sqrt(np.square(np.arange(-1, 3, 1) - min_val))
+        y_ticks = np.sqrt(np.square(np.arange(0, 3, 1) - min_val))
+        ax.set_ylim(0, y_ticks.max())
         ax.set_yticks(y_ticks)
-        ax.set_yticklabels([])
-        for i in range(4):
-            y = ytick_labels[i]
-            if i > 0:
-                ax.text(0.0, y, "z={}".format(i - 1), fontproperties=font_axis_prop)
-                ax.text(x_as[0], 3.3, subtext[0], fontproperties=font_prop, rotation=0, horizontalalignment='center')
-                ax.text(x_as[1], 3.65, subtext[1], fontproperties=font_prop, rotation=-40, horizontalalignment='center')
-                ax.text(x_as[2], 3.5, subtext[2], fontproperties=font_prop, rotation=-80, horizontalalignment='center')
-                ax.text(x_as[3], 3.5, subtext[3], fontproperties=font_prop, rotation=-120, horizontalalignment='center')
-                ax.text(x_as[4], 3.55, subtext[4], fontproperties=font_prop, rotation=-160, horizontalalignment='center')
-                ax.text(x_as[5], 3.5, subtext[5], fontproperties=font_prop, rotation=-200, horizontalalignment='center')
-                ax.text(x_as[6] + 0.1, 3.5, subtext[6], fontproperties=font_prop, rotation=-240, horizontalalignment='center')
-                ax.text(x_as[7], 3.55, subtext[7], fontproperties=font_prop, rotation=-280, horizontalalignment='center')
-                ax.text(x_as[8], 3.55, subtext[8], fontproperties=font_prop, rotation=-320, horizontalalignment='center')
+        ax.set_yticklabels([r"$z = 0$", r"$z = 1$", r"$z = 2$"])
+        ax.set_title(labels[i])
+
+    fig.suptitle(title)
+    fig.tight_layout()
     if savefig:
-        plt.savefig("image/radar_plot.pdf", format="pdf")
+        fig.savefig("radar_plot.pgf")
+    fig.show()
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('data/CountryLevelAMCEVals.csv')
-    create_d3_dendogram(df, leaf_color="Culture")
-    radar_plot(df)
+    _model_map = {
+        "falcon-7b-instruct": "Falcon 7B-Instruct",
+        "falcon-40b-instruct": "Falcon 40B-Instruct",
+        "falcon-180B-chat": "Falcon 180B-Chat",
+        "Llama-2-7b-chat-hf": "Llama 2 7B-Chat",
+        "Llama-2-13b-chat-hf": "Llama 2 13B-Chat",
+        "Llama-2-70b-chat-hf": "Llama 2 70B-Chat",
+        "Meta-Llama-3-8B-Instruct": "Llama 3 8B-Instruct",
+        "Meta-Llama-3-70B-Instruct": "Llama 3 70B-Instruct",
+        "mpt-7b-chat": "MPT 7B-Chat",
+        "mpt-30b-chat": "MPT 30B-Chat",
+    }
+    model_name = "Meta-Llama-3-70B-Instruct"
+    file_path = Path(__file__).parent.parent.parent.parent / "data" / "prepared_for_fig3" / f"{model_name}.csv"
+    df_loaded = pd.read_csv('CountriesChangePr.csv')
+    df = pd.DataFrame()
+    df["Country"] = df_loaded["Unnamed: 0"]
+    columns = [
+        "[Omission -> Commission]: Estimates",
+        "[Passengers -> Pedestrians]: Estimates",
+        "Gender [Male -> Female]: Estimates",
+        "Fitness [Large -> Fit]: Estimates",
+        "Law [Illegal -> Legal]: Estimates",
+        "Social Status [Low -> High]: Estimates",
+        "Age [Elderly -> Young]: Estimates",
+        "No. Characters [Less -> More]: Estimates",
+        "Species [Pets -> Humans]: Estimates",
+    ]
+    for column in columns:
+        df[column] = df_loaded[column]
+    # create_d3_dendogram(df, leaf_color="Culture")
+    radar_plot(_model_map[model_name], df)
