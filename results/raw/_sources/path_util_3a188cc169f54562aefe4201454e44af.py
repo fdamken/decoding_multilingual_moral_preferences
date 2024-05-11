3203686f2fd2ea0ce207bfb8e95bb0033dc90f66
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2024-05-11 14:56:43 +0200

    add raw gemini/gpt results

---
+++ results/raw/_sources/path_util_3a188cc169f54562aefe4201454e44af.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from typing import Final
+
+project_root_dir: Final[Path] = Path(__file__).parent.parent
+data_dir: Final[Path] = project_root_dir / "data"
+results_local_dir: Final[Path] = project_root_dir / "results_local"
+results_dir: Final[Path] = project_root_dir / "results"
+raw_experiment_results_dir: Final[Path] = results_dir / "raw"
+cleansed_experiment_results_dir: Final[Path] = results_dir / "cleansed"
+preprocessed_experiment_results_dir: Final[Path] = results_dir / "preprocessed"
+prepared_experiment_results_dir: Final[Path] = results_dir / "prepared"
+
+
+def get_data_path(language: str) -> Path:
+    path = data_dir / "preprocessed" / f"dataset_{language}.csv"
+    assert path.exists(), f"dataset for language '{language}' not found"
+    return path
